031df4077f1a43d4e59564fc17d9908f3decd0ee
Callym <callym.cooke@gmail.com>
2018-08-11 14:46:57 +0100

    Add MongoDB

--- backend/config/env/production.js
+++ backend/config/env/production.js
@@ -58,7 +58,7 @@ module.exports = {
       //  sails_datastores__default__url=mysql://admin:myc00lpAssw2D@db.example.com:3306/my_prod_db
       //  ```
       //--------------------------------------------------------------------------
-
+      adapter: 'sails-mongo',
       /****************************************************************************
       *                                                                           *
       * More adapter-specific options                                             *
